29bc4b99624d9d961ef1a1e803bd3eba7c0417ca
qinglusong <sql900211@163.com>
2018-09-03 19:41:55 +0800

    up

---
+++ cache/admin/header.htm.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version 2.6.26, created on 2018-09-03 19:31:07
+         compiled from header.htm */ ?>
+<div id="dcHead">
+ <div id="head">
+  <div class="logo"><a href="index.php" title="慧目堂网站管理系统">慧目堂网站管理系统</a></div>
+  <div class="box">
+   <ul class="siteName">
+    <?php echo $this->_tpl_vars['site']['site_name']; ?>
+
+   </ul>
+   <ul class="nav">
+    <li><a href="../index.php" target="_blank"><?php echo $this->_tpl_vars['lang']['top_go_site']; ?>
+</a></li>
+    <li><a href="index.php?rec=clear_cache"><?php echo $this->_tpl_vars['lang']['clear_cache']; ?>
+</a></li>
+    <li class="dropMenu"><a href="JavaScript:void(0);" class="parent"><?php echo $this->_tpl_vars['lang']['top_welcome']; ?>
+<?php echo $this->_tpl_vars['user']['user_name']; ?>
+</a>
+     <div class="menu"> <a href="manager.php?rec=edit&id=<?php echo $this->_tpl_vars['user']['user_id']; ?>
+"><?php echo $this->_tpl_vars['lang']['top_manager_edit']; ?>
+</a></div>
+    </li>
+    <li><a href="login.php?rec=logout"><?php echo $this->_tpl_vars['lang']['top_logout']; ?>
+</a></li>
+   </ul>
+  </div>
+ </div>
+</div>
+<!-- dcHead 结束 -->
---
+++ cache/case.html.php
@@ -0,0 +1,191 @@
+<?php /* Smarty version 2.6.26, created on 2018-09-03 19:36:33
+         compiled from case.html */ ?>
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'inc/header.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+    <link href="http://www.3e-d.com/theme/huimutang/images/css/case.css" rel="stylesheet">
+    <div class="bg_1">
+         <div class="h-btn-div qus-color1 h-btn-l pc-show wow fadeIn">
+             <div class="btn-back-div">
+                 <span><a href='case_1_0.html'>项目分类</a></span>
+                 <span><a href='case_2_0.html'>行业分类</a></span>
+                 <div class="btn-l show">项目分类</div>
+                 <div class="btn-r hidden">行业分类</div>
+             </div>
+         </div>
+        <div class="ul-div pc-show class-1 show">
+            <ul>
+                <li class="wow fadeInUp"><a class="active">品牌战略年度咨询</a></li>
+                <li class="wow fadeInUp" data-wow-delay="50ms"><a>品牌LOGO/VI设计</a></li>
+                <li class="wow fadeInUp" data-wow-delay="100ms"><a>商品包装&工业设计</a></li>
+                <li class="wow fadeInUp" data-wow-delay="150ms"><a>品牌营销/广告/影视</a></li>
+                <li class="wow fadeInUp" data-wow-delay="200ms"><a>互联网营销&网站</a></li>
+                <li class="wow fadeInUp" data-wow-delay="250ms"><a>品牌店面&环境指示</a></li>
+            </ul>
+        </div>
+        
+
+
+        <div class="m-btn-div mobile-show flex-show flex-row">
+            <div class="m-btn flex-show flex-row wow fadeInLeft " id="case_class_but"><div>案例分类</div><div class="btn-img-div"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/m/btn-d-s.png"></div></div>
+            <div class="m-btn flex-show flex-row wow fadeInRight" id="hy_class_but"><div>行业分类</div><div class="btn-img-div"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/m/btn-d-s.png"></div></div>
+        </div>
+
+        <div class="class-con">
+            <div class="bg-4-c flex-show flex-row">
+                <a href="news.html">
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="100ms">
+                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic3.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                </a>
+                <a href="news.html">
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="200ms">
+                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic1.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                </a>
+                    <a href="news.html">
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="300ms">
+                    <div class="bg-4m-img news-panel-focus"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic2.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                    </a>
+            </div>
+            <div class="bg-4-c flex-show flex-row">
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="100ms">
+                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic3.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="200ms">
+                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic1.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="300ms">
+                    <div class="bg-4m-img news-panel-focus"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic2.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+            </div>
+            <div class="bg-4-c flex-show flex-row">
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="100ms">
+                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic3.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="200ms">
+                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic1.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="300ms">
+                    <div class="bg-4m-img news-panel-focus"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic2.jpg"></div>
+                    <p class="bg-4m-t1">鼓起羽绒</p>
+                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="btn-down-div pc-show wow fadeIn">
+        <img src="http://www.3e-d.com/theme/huimutang/images/imgs/donw_btn.png">
+    </div>
+	
+	
+	
+	
+	
+	<div class="m-class-btn-div1  flex-show flex-row hidden">
+    <ul>
+        <li><a>金融保险</a></li>
+        <li><a>化妆品</a></li>
+        <li><a>珠宝首饰</a></li>
+        <li><a>制造业</a></li>
+        <li><a>地产建筑</a></li>
+        <li><a>生物医药</a></li>
+        <li><a>纺织服饰</a></li>
+        <li><a>文化传媒</a></li>
+        <li><a>互联网+</a></li>
+        <li><a>家居卫浴</a></li>
+        <li><a>教育培训</a></li>
+        <li><a>母婴保健</a></li>
+        <li><a>食品酒水</a></li>
+        <li><a>汽车&后市场</a></li>
+    </ul>
+    <div class="close-div">
+        <img src="http://www.3e-d.com/theme/huimutang/images/imgs/m/close_img.png">
+    </div>
+</div>
+<div class="m-class-btn-div2  flex-show flex-column hidden">
+    <ul>
+        <li><a>品牌战略年度咨询</a></li>
+        <li><a>品牌LOGO/VI设计</a></li>
+        <li><a>商品包装&工业设计</a></li>
+        <li><a>品牌营销/广告/影视</a></li>
+        <li><a>互联网营销&网站</a></li>
+        <li><a>品牌店面&环境指示</a></li>
+    </ul>
+    <div class="close-div">
+        <img src="http://www.3e-d.com/theme/huimutang/images/imgs/m/close_img.png">
+    </div>
+</div>
+
+<script>
+<?php echo '
+    $(".close-div").click(function () {
+        $(".m-class-btn-div1").addClass("hidden");
+        $(".m-class-btn-div2").addClass("hidden");
+    })
+
+    $("#case_class_but").click(function () {
+        $(".m-class-btn-div1").removeClass("hidden");
+    })
+    $("#hy_class_but").click(function () {
+        $(".m-class-btn-div2").removeClass("hidden");
+    })
+	
+	
+	
+	
+	//houjia
+	
+	 $(".btn-down-div").click(function () {
+        var newhtml=\'<div class="bg-4-c flex-show flex-row">\\n\' +
+            \'                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="100ms">\\n\' +
+            \'                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic3.jpg"></div>\\n\' +
+            \'                    <p class="bg-4m-t1">鼓起羽绒</p>\\n\' +
+            \'                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>\\n\' +
+            \'                </div>\\n\' +
+            \'                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="200ms">\\n\' +
+            \'                    <div class="bg-4m-img news-panel-focus-img"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic1.jpg"></div>\\n\' +
+            \'                    <p class="bg-4m-t1">鼓起羽绒</p>\\n\' +
+            \'                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>\\n\' +
+            \'                </div>\\n\' +
+            \'                <div class="bg-4-c-m wow fadeInUp news-panel-focus" data-wow-delay="300ms">\\n\' +
+            \'                    <div class="bg-4m-img news-panel-focus"><img src="http://www.3e-d.com/theme/huimutang/images/imgs/pic2.jpg"></div>\\n\' +
+            \'                    <p class="bg-4m-t1">鼓起羽绒</p>\\n\' +
+            \'                    <p class="bg-4m-t2">高端羽绒家纺领军品牌</p>\\n\' +
+            \'                </div>\\n\' +
+            \'            </div>\'
+
+
+    $(".class-con").append(newhtml);
+
+    })
+	
+	
+'; ?>
+
+</script>
+    <!-- footer -->
+   <?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'inc/footer.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
